766874ce2ea87d372e459dfcdc2e462966c05870
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2022-07-06 15:22:59 +0530

    cart application

--- src/App.js
+++ src/App.js
@@ -1,32 +1,73 @@
 import './App.css';
-import { useState } from 'react';
 import { useReducer } from 'react';
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'INCREMENT':
+    case 'ADD_TO_CART':
       return {
-        type: 'INCREMENT',
-        count: state.count + 1,
+        product: state.product,
+        cart: [...state.cart, action.payload],
+        total: state.total + action.payload.price
       }
-      case 'DECREMENT':
+      break;
+    case 'REMOVE_FROM_CART':
       return {
-        type: 'DECREMENT',
-        count: state.count - 1,
+        product: state.product,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+        total: state.total - action.payload.price,
       }
-      default: return state
+      break; 
+    default: return state;
   }
-
 }
 
 function App() {
-  let [timer, setTimer] = useState(0);
-  const [state, dispatch] = useReducer(reducer, { type: 'INCREMENT', count:0 })
+  const [state, dispatch] = useReducer(reducer, {
+    product: [
+      {
+        id: 1,
+        name: 'Product 1',
+        price: 1000,
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        price: 2000,
+      },
+      {
+        id: 3,
+        name: 'Product 3',
+        price: 3000,
+      },
+    ],
+    cart: [],
+    total: 0,
+  })
   return (
     <div className="App">
-      <button onClick={() => dispatch({ type: 'INCREMENT' })} >Incr</button>  
-      <button onClick={() => dispatch({ type: 'DECREMENT' })} >Decr</button>
-      {state.count}
+      {
+        state.product.map(item => {
+          return (
+            <div key={item.id}>
+              {item.name} - <button onClick={() => {
+                dispatch({ type: 'ADD_TO_CART', payload: item })
+              }} >Add to Cart</button>
+            </div>
+          )
+        })
+      }
+      <div>
+        {state.cart.map(item => {
+          return (
+            <div key={item.id}>
+              {item.name} - <button onClick={() => {
+                dispatch({ type: 'REMOVE_FROM_CART', payload: item })
+              }} >Remove from Cart</button>
+            </div>
+          )
+        })}
+      </div>
+      {state.total}
     </div>
   );
 }
